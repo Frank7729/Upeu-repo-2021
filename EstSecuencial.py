e7302f3f629a2c8ee723ab3f35c949bfa8885e45
Frank7729 <>
2021-05-10 20:23:05 +0000

    Examen Unidad I

---
+++ EstSecuencial.py
@@ -0,0 +1,36 @@
+#Definir variables y otros
+print("Ejemplo estructura secuencial en Python")
+#Datos de entrada
+e1=int(input("Calificación Eval 01:"))
+e2=int(input("Calificación Eval 02:"))
+e3=int(input("Calificación Eval 03:"))
+e4=int(input("Calificación Eval 04:"))
+#Proceso
+promediofinal=(e1*0.25+e2*0.25+e3*0.25+e4*0.25)
+#Datos de salida
+print("La nota final es:", promediofinal)
+
+def Ejercicios04FGCM():
+  #Definir variables y otros
+  print("--> EJERCICIO 04 <--")
+  operadorX=0
+  resultado=0
+  #Datos de entrada
+  operadorX=input("El signo es:")
+  n1=int(input("Ingrese el 1er numero:"))
+  n2=int(input("Ingrese el 2do numero:"))
+  #Proceso
+  if operadorX=="suma" or operadorX=="+":
+  resultado=n1+n2
+  elif operadorX="Resta" or "-":
+  resultado=n1-n2
+  elif operadorX="Multiplicacion" or "*":
+  resultado=n1*n2
+  elif operadorX="Division" or "/":
+  resultado=n1/n2
+  elif operadorX="Potencia" or "**":
+  resultado=n1**n2
+  #Datos de salida
+  print("La resultado es:", resultado)
+Ejercicios04FGCM()
+print("")

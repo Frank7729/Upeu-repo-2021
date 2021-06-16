13a01c1690df02dfa8781c8e42c613883456ec9d
Frank7729 <frankchunca@gmail.com>
2021-06-16 10:24:01 -0500

    Avance en clases sobre Sist.Ventas

--- ProyectoVentasG2/src/main/java/pe/edu/upeu/App.java
+++ ProyectoVentasG2/src/main/java/pe/edu/upeu/App.java
@@ -1,13 +1,35 @@
 package pe.edu.upeu;
 
+import pe.edu.upeu.gui.MainGUI;
+import pe.edu.upeu.modelo.CategoriaTO;
+import pe.edu.upeu.modelo.ProductoTO;
+import pe.edu.upeu.util.LeerTeclado;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App{
+    public static void registrarCategoria(CategoriaTO categ){
+        System.out.println("------Registro de Categoria de Productos----");
+        System.out.println("IDCateg:"+categ.getIdCateg()+ "\tNomnbre:"+categ.getNombre());
+    
+    }
+
+
+
+
+    public static void main( String[] args ){   
         System.out.println( "Hello World!" );
+        LeerTeclado lt=new LeerTeclado();
+        CategoriaTO ca=new CategoriaTO();
+
+        ca.setIdCateg(lt.leer("","Ingrese el Id Categoria"));
+        ca.setNombre(lt.leer("","Ingrese nombre categoria:"));
+        registrarCategoria(ca);
+
+
+        new MainGUI();
     }
 }
+
